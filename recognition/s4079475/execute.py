493785e3008a2adf03fd413fe10dfd905f594706
John_proposal_grape <john.cominos@uqconnect.edu.au>
2021-10-17 21:08:37 +1000

    Working output. Adding dice calculations. modifying hyperparameters. Unvalidated.

--- recognition/s4079475/execute.py
+++ recognition/s4079475/execute.py
@@ -4,63 +4,100 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models, regularizers
 from tensorflow.keras.layers.experimental import preprocessing
+from sklearn.model_selection import train_test_split
 from tensorflow.keras import mixed_precision
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical, Sequence
 import project as p
+import model as m
 import os
 import matplotlib.pyplot as plt
+from PIL import Image
+import math
 import random
+
+def encode_y(y):
+    
+    y = tf.keras.utils.to_categorical(y, num_classes=2)
+    return y
+
+# A class that can be parsed in to the fit model parameter
+# to have its functions called natively by fit model. 
+#code ref
+#https://www.tensorflow.org/api_docs/python/tf/keras/utils/Sequence
+
+class ISICs2018Sequence(Sequence):
+    
+    def __init__(self, x, y, batchsize):
+        
+        self.x = x
+        self.y = y
+        self.batchsize = batchsize
+        
+    def __len__(self):
+        
+        return math.ceil(len(self.x) / self.batchsize)
+    
+    def __getitem__(self, id):
+        
+        x_names = self.x[id * self.batchsize:(id + 1) * self.batchsize]
+        y_names = self.y[id * self.batchsize:(id + 1) * self.batchsize]
+        
+        x_batch = list()
+        y_batch = list()
+        
+        for name in x_names :
+            
+            file_name = name[:len(name) - 4]
+            
+            train_image = np.asarray(Image.open("ISIC2018_Task1-2_Training_Input_x2/" + file_name + ".jpg").resize((256, 192))) / 255.0
+        
+            x_batch.append(train_image)
+        
+            ground_image = np.asarray(Image.open("ISIC2018_Task1_Training_GroundTruth_x2/" + file_name + "_segmentation" + ".png").resize((256, 192))) / 255.0 
+           
+            y_batch.append(ground_image)
+
+        x_batch = np.array(x_batch)
+        y_batch = np.array(y_batch)
+        
+        y_batch = encode_y(y_batch)
+        
+        return x_batch, y_batch
 
 def load_data(directory, seed) :
     
-    loaded = [file for file in os.listdir(directory) 
-            if file != "ATTRIBUTION.txt" and file != "LICENSE.txt"]
-
+    loaded = list()
+    
+    for file in os.listdir(directory) :
+    
+        if file != "ATTRIBUTION.txt" and file != "LICENSE.txt" :
+            
+            loaded.append(file)
 
     random.seed(seed)
     random.shuffle(loaded)
 
     return loaded
 
-def load_segment(image_shape, segment_file):
+def show_images(test, model) :
     
-    segment = tf.io.read_file(segment_file)
-    segment = tf.image.decode_jpeg(segment, channels=1)
-    segment = tf.image.resize(segment, image_shape)
-    segment = tf.cast(segment, tf.float32)
-    segment =  segment / 255.0
-    return segment
-
-def load_image(image_shape, image_file):
-
-    print(image_file, "image file")
-    print(image_shape, "image shape")
-
-    image = tf.io.read_file(image_file)
-    image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, image_shape)
-    image = tf.cast(image, tf.float32)
-    image = image / 255.0
-    return image
-
-def map_images(image_file, seg_file, image_shape=(192, 256)):
+    testlength = len(test)
+    rand = random.randint(0, testlength)
+    x, y = test.__getitem__(rand)
+    prediction = model.predict(x)
+    plt.figure(figsize=(10, 10))
     
-    image = load_image(image_shape, image_file)
-    seg = load_segment(image_shape, seg_file)
-    
-    return image, seg
-
-def view_images(dataset, n):
-    
-    plt.figure(figsize=(8, n*4))
-    i = 0
-
-    for img, label in dataset.take(n):
-        plt.subplot(n, 2, 2*i + 1)
-        plt.imshow(img)
-        plt.subplot(n, 2, 2*i + 2)
-        plt.imshow(label, cmap='gray')
-        i = i + 1
+    for i in range(8):
+        plt.subplot(10, 10, i*10+2)
+        plt.imshow(x[i])
+        plt.title("Original Image", size=4)
+        plt.subplot(10, 10, i*10+4)
+        plt.imshow(tf.argmax(prediction[i], axis=2))
+        plt.title("Model Output", size=4)
+        plt.subplot(10, 10, i*10+6)
+        plt.imshow(tf.argmax(y[i], axis=2))
+        plt.title("What it should be", size=4)
+    plt.show()
 
 if __name__ == "__main__":
 
@@ -69,42 +106,56 @@ if __name__ == "__main__":
     #test = load_data("D:/3710sets/ISIC_2019_Test_Input/ISIC_2019_Test_Input", seed)
     ground = load_data("D:/3710sets/2018/ISIC2018_Task1_Training_GroundTruth_x2", seed)
     
-    num_classes = 4 #find this out?
-    
     #creating a validation split
-    num_images = len(train)
+    #num_images = len(train)
     #num_labels = len(test_names)
     
-    val_split = 20 #20% for a multiple of 8
-    num_val_images = int(num_images * (val_split/100))
-    val_images = train[ : num_val_images] 
-    val_seg = ground[ : num_val_images]
+    #val_split = 20 #20% for a multiple of 8
+    #num_val_images = int(num_images * (val_split/100))
+    #val_images = train[ : num_val_images] 
+    #val_seg = ground[ : num_val_images]
     
-    train_images = train[num_val_images :]
-    train_seg = train[num_val_images :]
+    #train_images = train[num_val_images :]
+    #train_seg = ground[num_val_images :]
      
-    train_set = tf.data.Dataset.from_tensor_slices((train_images, train_seg))
-    test_set = tf.data.Dataset.from_tensor_slices((val_images, val_seg))
+    #train_set = tf.data.Dataset.from_tensor_slices((train_images, train_seg))
+    #test_set = tf.data.Dataset.from_tensor_slices((val_images, val_seg))    
     
-    train_set = train_set.map(map_images)
-    test_set = test_set.map(map_images)
-    
-    #for element in train_set :
-        #print(element)
+    #train_set = train_set.map(map_images)
+    #test_set = test_set.map(map_images)
 
+    
     #suffle datasets?
 
     #train_labels = to_categorical(train_labels, num_classes)
     #test_labels = to_categorical(test_labels, num_classes)
     #val_labels = to_categorical(val_labels, num_classes)
+    
+    #print(len(train_images), "train shape")
+    #print(len(train_seg), "train shape")    
+    
+    #print(train_images[:3], "three train images")
+    #print(train_seg[:3], "three seg images")
+       
+    #view_images(train_images, 3)
+    #plt.show()
 
-    view_images(train_set, 3)
-    plt.show()
+    train_images, test_images, ground_images, ground_test = train_test_split(train, ground, test_size=0.20, random_state=50)
+
+    train_images, val_images, ground_images, ground_val = train_test_split(train_images, ground_images, test_size=0.20, random_state=50)
+
+    train = ISICs2018Sequence(train_images, ground_images, 8)
+    val = ISICs2018Sequence(val_images, ground_val, 8)
+    test = ISICs2018Sequence(test_images, ground_test, 8)
     
     model = p.UNET()
     
-    model.evaluate(train_set)
+    print(len(train),  "length train")
     
-    model.fit(train_names,
-              validation_data=(val_images),
-              epochs=epochs, verbose=1, workers=4)
+    model.fit(train,
+          validation_data=val,
+          epochs=5, verbose=1, workers=4)
+    
+    model.evaluate(test)
+    
+    show_images(test, model)
--- recognition/s4079475/project.py
+++ recognition/s4079475/project.py
@@ -7,12 +7,25 @@ from tensorflow.keras import datasets, layers, models, regularizers
 from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras import mixed_precision
 
+def dice(true, predicted):
 
+    true = tf.keras.backend.flatten(true)
+    predicted = tf.keras.backend.flatten(predicted)
+    sum = tf.keras.backend.sum(predicted * true)
+    return (2.0 * sum)/tf.keras.backend.sum(predicted + true)
+
+def dice_foreground(true, predicted):
+    
+    return dice(true[:,:,1], predicted[:,:,1])
+
+def dice_background(true, predicted):
+    
+    return dice(true[:,:,0], predicted[:,:,0])
 
 def feature_conv_layer(input, filters, stride=1, size=(3, 3)) :
     
     result = tf.keras.layers.Conv2D(filters, size, stride, padding='same')(input)
-    result = tf.keras.layers.LeakyReLU(alpha=0.01)(result)
+    result = tf.keras.layers.ReLU()(result)
     return result
 
 def extraction_layer(input, filters) :
@@ -28,6 +41,11 @@ def upsample_layer(input, filters, inputTwo) :
     result = feature_conv_layer(result, filters)
     return tf.keras.layers.concatenate([result, inputTwo])
 
+def downsample_layer(input) :
+    
+    result = tf.keras.layers.MaxPooling((2, 2))(input)
+    return result
+
 def refine_layer(input, filters) :
     
     result = feature_conv_layer(input, filters)
@@ -38,10 +56,9 @@ def UNET () :
 
     input = tf.keras.layers.Input(shape=(192, 256, 3))
 
+    #although paper says start with 16 seem to get better results starting with 8? 
     filters = 8    
     current = feature_conv_layer(input, filters)
-    
-    depth = 4
     
     extract1 = extraction_layer(current, filters)
     filters = filters * 2
@@ -69,27 +86,30 @@ def UNET () :
     current = upsample_layer(current, filters, extract3)
 
     current = refine_layer(current, filters)
-    segment = feature_conv_layer(current, 16, size=(1, 1))
-    segment = tf.keras.layers.UpSampling2D((2, 2))(segment)
     filters = filters/2
+    segment = feature_conv_layer(current, filters, size=(1, 1))
+    segment = tf.keras.layers.UpSampling2D((2, 2))(segment)
     current = upsample_layer(current, filters, extract2)
     
     current = refine_layer(current, filters)
+    filters = filters/2
     segment2 = feature_conv_layer(current, 16, size=(1, 1))
     segment2 = tf.keras.layers.add([segment, segment2])
     segment2 = tf.keras.layers.UpSampling2D((2, 2))(segment2)
-    filters = filters/2
     current = upsample_layer(current, filters, extract1)    
 
     result = feature_conv_layer(current, 32)
     segment3 = feature_conv_layer(result, 16, size=(1, 1))
     segment3 = tf.keras.layers.add([segment3, segment2])
     
-    result = tf.keras.layers.Conv2D(2, (1, 1), padding = "same", activation="softmax")(segment3)
+    result = tf.keras.layers.Conv2D(filters, (1, 1), padding = "same", activation="softmax")(segment3)   
     
     model = tf.keras.Model(inputs=input, outputs=result)
+    
+    model.summary()
+    
     model.compile(loss= tf.keras.losses.CategoricalCrossentropy(),
                   optimizer='adam',
-                  metrics=['accuracy'])
+                  metrics=['accuracy', dice_foreground])
                   
     return model             
